36e580e7f383020db517585db9be7d38934994fe
Heinrich Reinking <hreinkinge@gmail.com>
2020-11-04 10:26:04 -0300

    update cert-manager CRDs

--- ruka/cert-manager/cm-and-clusterissuer.sh
+++ ruka/cert-manager/cm-and-clusterissuer.sh
@@ -4,7 +4,7 @@ set -ex
 
 kubectl create ns cert-manager
 helm repo add jetstack https://charts.jetstack.io
-kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.11/deploy/manifests/00-crds.yaml --validate=false
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.0.4/cert-manager.yaml
 helm install cert-manager -n cert-manager jetstack/cert-manager
 cat > secret.yaml << END
 apiVersion: v1
